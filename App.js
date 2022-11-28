5e69eb5c6746932544362a3d1e548c054baa5aff
Momal Afzal <momal@Momals-MacBook-Pro.local>
2022-11-28 16:20:42 +0500

    first commit

--- App.js
+++ App.js
@@ -1,117 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from 'react'
+import { Image, Platform } from 'react-native'
+import { TabNavigator } from 'react-navigation'
+import { PeopleScreen } from './screens/PeopleScreen'
+import { DecisionScreen } from './screens/DecisionScreen'
+import { View } from 'react-native'
+const platformOS = Platform.OS.toLowerCase()
+import Icon from 'react-native-vector-icons/FontAwesome'
+const tabs = TabNavigator(
+    {
+        PeopleScreen: {
+            screen: PeopleScreen,
+            navigationOptions: {
+                tabBarLabel: 'People',
+                tabBarIcon: ({ tintColor }) => (
+                    <Icon name="rocket" size={30} color="#900" />
+                ),
+            },
+        },
+        DecisionScreen: {
+            screen: DecisionScreen,
+            navigationOptions: {
+                tabBarLabel: 'Decision',
+                tabBarIcon: ({ tintColor }) => (
+                    <Icon name="rocket" size={30} color="#900" />
+                ),
+            },
+        },
+        RestaurantScreen: {
+            screen: RestaurantScreen,
+            navigationOptions: {
+                tabBarLabel: 'Restaurants',
+                tabBarIcon: ({ tintColor }) => (
+                    <Icon name="rocket" size={30} color="#900" />
+                ),
+            },
+        },
+    },
+    {
+        initialRouteName: 'DecisionScreen',
+        animationEnabled: true,
+        swipeEnabled: true,
+        backBehavior: 'none',
+        lazy: true,
+        /* Tabs go on top for Android, bottom for iOS. */
+        tabBarPosition: platformOS === 'android' ? 'top' : 'bottom',
+        tabBarOptions: {
+            activeTintColor: '#ff0000',
+            showIcon: true,
+            /* Tabs on Android are overlapped by the status bar, so add some */
+            /* padding to fix that. */
+            style: {
+                paddingTop:
+                    platformOS === 'android' ? Constants.statusBarHeight : 0,
+            },
+        },
+    } /* End options. */
+)
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default tabs
---
+++ components/CustomButton.js
@@ -0,0 +1,59 @@
+import { TouchableOpacity, View, Text } from 'react-native'
+import React, { Component } from 'react'
+import { PropTypes } from 'prop-types'
+
+class CustomButton extends Component {
+    render() {
+        const { text, onPress, buttonStyle, textStyle, width, diabled } =
+            this.props
+
+        return (
+            <TouchableOpacity
+                style={[
+                    {
+                        padding: 10,
+                        height: 60,
+                        borderRadius: 8,
+                        margin: 10,
+                        width: width,
+                        backgroundColor:
+                            disabled != null && disabled === 'true'
+                                ? '#e0e0e0'
+                                : '#303656',
+                    },
+                    buttonStyle,
+                ]}
+                onPress={() => {
+                    if (disabled == null || disabled === 'false') {
+                        onPress()
+                    }
+                }}
+            >
+                <Text
+                    style={[
+                        {
+                            fontSize: 20,
+                            fontWeight: 'bold',
+                            color: '#ffffff',
+                            textAlign: 'center',
+                            paddingTop: 8,
+                        },
+                        textStyle,
+                    ]}
+                >
+                    {text}
+                </Text>
+            </TouchableOpacity>
+        )
+    }
+}
+CustomButton.propTypes = {
+    text: PropTypes.string.isRequired,
+    onPress: PropTypes.func.isRequired,
+    buttonStyle: PropTypes.object,
+    textStyle: PropTypes.object,
+    width: PropTypes.string,
+    disabled: PropTypes.string,
+}
+
+export default CustomButton
